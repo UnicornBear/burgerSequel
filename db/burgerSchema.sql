ea54d6b4c6ea9eff449f519e8c9ce304ff5cfaf3
UnicornBear <jtbregenzer@gmail.com>
2019-06-24 17:03:28 -0600

    updated sql for new heroku jawsdb

---
+++ db/burgerSchema.sql
@@ -0,0 +1,27 @@
+USE qm70izqypmecktl0;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	CustomerId int,
+  	PRIMARY KEY(id)
+);
+
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffaloe Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Fire Cracker', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Club Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hot Mamma Mushroom and Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Colorado Green Chili Buffalo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monterrey Ham Hawaiin Pineapple', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Almost Gone Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ate the Whole Thing', true);
